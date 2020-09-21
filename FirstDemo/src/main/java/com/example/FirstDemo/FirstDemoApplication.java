4c045df38e47fe71cf6bef854cd5d63547298d6a
spras <spras@192.168.1.5>
2020-09-21 17:55:45 +0530

    First Commit
    Java Project Sample

---
+++ FirstDemo/src/main/java/com/example/FirstDemo/FirstDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.FirstDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDemoApplication.class, args);
+		System.out.println("Pillaiyar Thunai, Murugan Thunai");
+		
+	}
+
+}
